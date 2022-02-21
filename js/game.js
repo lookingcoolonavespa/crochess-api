02164e9f0ace009c9b4305420363f134fa4cc20c
alex <alexlhliang@gmail.com>
2022-02-20 20:35:22 -0800

    added moves

---
+++ js/game.js
@@ -0,0 +1,24 @@
+/*
+- need pieces
+- need gameboard
+- need to control game
+
+pieces need to:
+- have moveset
+
+gameboard module needs to: 
+- keep track of where pieces are
+- calculate possible moves
+- need to keep track of en passant
+- castling
+- need to keep track of whether pawn can advance two squares
+
+game module needs to:
+- control turn
+- timer
+- announce checks 
+- 
+
+
+- 
+*/
---
+++ js/moves.js
@@ -0,0 +1,22 @@
+const moves = {
+  lateral:
+    ({ x: x1, y: y1 }) =>
+    ({ x: x2, y: y2 }) =>
+      x1 === x2 || y1 === y2,
+  diagonal:
+    ({ x: x1, y: y1 }) =>
+    ({ x: x2, y: y2 }) =>
+      Math.abs(x2 - x1) === Math.abs(y2 - y1),
+  xByN:
+    (num) =>
+    ({ x: x1 }) =>
+    ({ x: x2 }) =>
+      Math.abs(x1 - x2) === num,
+  yByN:
+    (num) =>
+    ({ y: y1 }) =>
+    ({ y: y2 }) =>
+      Math.abs(y1 - y2) === num,
+};
+
+export default moves;
