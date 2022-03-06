0dd4a4cb472b1140ecf0ab1a1cf6ec3f976cd65f
alex <alexlhliang@gmail.com>
2022-03-06 14:42:09 -0800

    trying to get jest to work

---
+++ tests/game.test.js
@@ -0,0 +1,16 @@
+import game from '../modules/game';
+
+import { test, expect } from 'jest';
+
+test('init places pieces correctly', () => {
+  game.init();
+
+  const board = game.gameboard.board;
+  const cols = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+  cols.forEach((col) => {
+    expect(board[col.concat(2)].piece.type).toBe('pawn');
+    expect(board[col.concat(2)].piece.color).toBe('white');
+    expect(board[col.concat(7)].piece.type).toBe('pawn');
+    expect(board[col.concat(7)].piece.color).toBe('black');
+  });
+});
